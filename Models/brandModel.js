5317d75a35f91410191bc238a201a0bda2a3ed96
Mohmed-saleh1 <mu.saleh616@gmail.com>
2023-07-31 20:38:43 +0300

    create the brand schema and model

---
+++ Models/brandModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const brandSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Please Enter you name '],
+        minlength:[5,  'the name is too short'],
+        maxlength:[30, 'the name is too long'],
+        unique:[true,  'the brand should be unique']
+    },
+    slug:{
+        type:String,
+        lowercase:true,
+    },
+    image:String,
+},{timestamp:true})
+
+const brandModel =  mongoose.model('Categories',brandSchema);
+ 
+module.exports={brandSchema,brandModel}
