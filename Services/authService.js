c0495c88da5d5580840249d635e2be11ae33cefc
Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>
2023-12-05 23:29:38 +0200

    adding protect service

--- Services/authService.js
+++ Services/authService.js
@@ -20,11 +20,31 @@ exports.signup = asyncHandler(async (req, res, next) => {
 
 exports.login = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
-  if (!user || (!await bcrypt.compare(req.body.password, user.password))) {
+  if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
     return next(new ApiError("Incorrect email or password"));
   }
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRE_TIME,
   });
   res.status(201).json({ data: user, token: token });
+});
+
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+  // console.log(req.headers);
+  if (req.headers.authorization) {
+    token = req.headers.authorization;
+  }
+  if (!token) {
+    return next(new ApiError("You are not loged in ", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  console.log(decoded);
+
+  const currentuser = await User.findById(decoded.userId);
+  if (!currentuser) {
+    return next(new ApiError("ther user for this token not exist ", 401));
+  }
+  next();
 });
